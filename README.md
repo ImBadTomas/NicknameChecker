0876bf2e4f0f425faed717e400fe90d9196f62af
Tomáš <57567741+ImBadTomas@users.noreply.github.com>
2023-08-28 17:33:40 +0200

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NicknameChecker
+Simple Javascript file, that will check if your Minecraft username is taken or not
+
+You need **Node.js** for this (i used 18.x)
--- checker.js
+++ checker.js
@@ -7,8 +7,8 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log(chalk.white("We are currently supporti Discord, Minecraft checker"));
-console.log(chalk.white("We are using MojangAPI and LixqaAPI"));
+console.log(chalk.white("Minecraft Username checker :O"));
+console.log(chalk.white("We are using MojangAPI"));
 checker()
 
 function checker() {
@@ -22,33 +22,12 @@ function checker() {
     https.get(`https://api.mojang.com/users/profiles/minecraft/${nickname}`, (response) => {
       if (response.statusCode === 404) {
         console.log(chalk.green(`[INFO] ${nickname} is not taken on Minecraft`));
+        process.exit(1);
       } else {
         console.log(chalk.red(`[INFO] ${nickname} is taken on Minecraft`));
-      }
-  
-      https.get(`https://api.lixqa.de/v3/discord/pomelo/${nickname}`, (response) => {
-        let data = '';
-  
-        response.on('data', (chunk) => {
-          data += chunk;
-        });
-  
-        response.on('end', () => {
-          const dataObj = JSON.parse(data);
-          const status = dataObj.data.check.status;
-  
-          if (status === 2) {
-            console.log(chalk.green(`[INFO] ${nickname} is not taken on Discord`));
-            checker()
-          } else {
-            console.log(chalk.red(`[INFO] ${nickname} is taken on Discord`));
-            checker()
-          }
-        });
-      }).on('error', (error) => {
-        console.log(chalk.red(`An error occurred while accessing Lixqa API: ${error}`));
         process.exit(1);
-      });
+      }
+
     }).on('error', (error) => {
       console.log(chalk.red(`An error occurred while accessing Minecraft API: ${error}`));
       process.exit(1);
